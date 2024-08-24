e98e3d7a141281a2c7066a41708f0995b56dd434
tanvir35web <islammdtanvirul@gmail.com>
2024-08-24 10:49:04 +0600

    validation error fixed on user signup api

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const User = require("../models/user");
 const { setUser } = require("../services/auth");
-const { errorHandleFormater } = require("../services/errorHandlerFormat");
 const { body, validationResult } = require('express-validator');
 
 
@@ -61,29 +60,49 @@ const validateUserSignup = [
 async function handleUserSignUp(req, res) {
 
   const errors = validationResult(req);
-  errorHandleFormater(req, res, errors);
+  if (!errors.isEmpty()) {
+    const groupedErrors = errors.array().reduce((acc, err) => {
+        if (!acc[err.path]) {
+            acc[err.path] = [];
+        }
+        acc[err.path].push(err.msg);
+        return acc;
+    }, {});
 
+    const firstErrorMessage = errors.array()[0].msg;
+    const additionalErrorsCount = errors.array().length - 1;
+    const summaryMessage = additionalErrorsCount > 0
+        ? `${firstErrorMessage} (and ${additionalErrorsCount} more error${additionalErrorsCount > 1 ? 's' : ''})`
+        : firstErrorMessage;
+
+    return res.status(422).json({
+        message: summaryMessage,
+        errors: groupedErrors,
+    });
+}
   const { name, email, password } = req.body;
-  
-  // Check if email already exists
-  const isExistingUser = await User.findOne({ email });
-  if (isExistingUser) {
-    return res.status(400).json({ message: 'Email already exists!' });
-  }
-  //checked password length
-  if (password && password.length < 6) {
-    return res.status(400).json({ message: 'Password should be at least 6 characters long!' });
-  }
+
+
   try {
+    // Check if email already exists
+    const isExistingUser = await User.findOne({ email });
+    if (isExistingUser) {
+      return res.status(400).json({ message: 'Email already exists!' });
+    }
+    //checked password length
+    if (password && password.length < 6) {
+      return res.status(400).json({ message: 'Password should be at least 6 characters long!' });
+    }
+
     const user = new User({
-      name, 
+      name,
       email,
       password,
     });
     await User.create(user);
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    console.error(error);
+    console.error('myError', error);
     res.status(500).json({ message: 'Server Error!' });
   }
 }
