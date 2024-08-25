36f9e6428e9034e45bce0ce018b6d8933a3ba989
tanvir35web <islammdtanvirul@gmail.com>
2024-08-25 23:10:22 +0600

    cord updated

--- index.js
+++ index.js
@@ -40,7 +40,16 @@ const allowedOrigins = [
   // add more origins here
 ];
 
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, 
+}));
 
 
 //routes
