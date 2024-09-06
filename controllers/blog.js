90ceebbf44f11eb7a95a3e3a1e6d2a36263aa785
tanvir35web <islammdtanvirul@gmail.com>
2024-09-06 19:10:45 +0600

    blog update and delete api implemented

--- controllers/blog.js
+++ controllers/blog.js
@@ -8,7 +8,7 @@ async function handleGetAllBlogs(req, res) {
     res.status(200).json({ message: 'All blogs fetched successfully', data: blogs });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error!' });
+    res.status(500).json({ message: 'Server Error from all blog fetch!' });
   }
 }
 
@@ -32,7 +32,7 @@ async function handleCreateBlog(req, res) {
     res.status(201).json({ message: 'Blog created successfully', data: savedBlog });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error!' });
+    res.status(500).json({ message: 'Server Error from blog creation!' });
   }
 };
 
@@ -45,7 +45,36 @@ async function handleGetBlogById(req, res) {
     res.status(200).json({ message: 'Blog fetched successfully by ID', data: blog });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error!' });
+    res.status(500).json({ message: 'Server Error from get blog by id!' });
+  }
+};
+
+async function handleUpdateBlogById(req, res) {
+  const hasErrors = errorValidationMessageFormatter(req, res);
+  if (hasErrors) return; // Stop further execution if there are validation errors
+
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found!' });
+    }
+    res.status(200).json({ message: 'Blog updated successfully', data: blog });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error from Blog update!' });
+  }
+};
+
+async function handleDeleteBlogById(req, res) {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: 'Blog not found!' });
+    }
+    res.status(200).json({ message: 'Blog deleted successfully by ID' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server Error from Blog delete!' });
   }
 };
 
@@ -56,4 +85,6 @@ module.exports = {
   handleGetAllBlogs,
   handleCreateBlog,
   handleGetBlogById,
+  handleUpdateBlogById,
+  handleDeleteBlogById,
 };
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const { handleGetAllBlogs, handleGetBlogById, handleCreateBlog } = require('../controllers/blog');
+const { handleGetAllBlogs,
+  handleGetBlogById,
+  handleCreateBlog,
+  handleUpdateBlogById,
+  handleDeleteBlogById
+} = require('../controllers/blog');
 const { validateBlogPost } = require('../errorValidation/validationMassages');
 const router = express.Router();
 
 
 router.get('/', handleGetAllBlogs);
 router.post('/', validateBlogPost, handleCreateBlog);
-router.get('/:id', handleGetBlogById );
+router.get('/:id', handleGetBlogById);
+router.patch('/:id', handleUpdateBlogById);
+router.delete('/:id', handleDeleteBlogById);
 
 
 module.exports = router;
